a4c10429c6d77f7f1b9d7100bc5dbda3260ee0fc
Ubuntu <ubuntu@ip-172-31-57-171.ec2.internal>
2021-04-27 17:57:42 +0000

    added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Copyright (c) 2021 Layla Michel All rights reserved.
+//
+// Created by: Layla Michel
+// Date: April 27, 2021
+// This program displays "Hello, World!" and follows correct c++ style.
+
+int main() {
+    std::cout << "Hello, World! With Style" << std::endl;
+}
